60678ff0669f661f72611f9e289a682ac96da29b
misogihagi <hagimiso@gmail.com>
2021-12-14 01:28:04 +0900

    added exam 3

---
+++ src/exam3.js
@@ -0,0 +1,125 @@
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+const filepath = argv.p || "test/test.csv";
+const timeoutCountMax = argv.N || 3;
+const responseCount = argv.m || 5;
+const responseAvg = argv.t || 100;
+
+const csvStr = require("fs").readFileSync(filepath).toString();
+
+const stdout = [];
+
+function formatDateStr(s) {
+  return (
+    s.slice(0, 4) +
+    "-" +
+    s.slice(4, 6) +
+    "-" +
+    s.slice(6, 8) +
+    " " +
+    s.slice(8, 10) +
+    ":" +
+    s.slice(10, 12) +
+    ":" +
+    s.slice(12, 14)
+  );
+}
+function outputDateStr(d) {
+  return (
+    d.getFullYear() +
+    "年" +
+    (d.getMonth() + 1) +
+    "月" +
+    d.getDate() +
+    "日" +
+    d.getHours() +
+    "時" +
+    d.getMinutes() +
+    "分" +
+    d.getSeconds() +
+    "." +
+    d.getMilliseconds().toString().padStart(3, "0") +
+    "秒"
+  );
+}
+
+const csvArray = csvStr.split(require("os").EOL).map((s) => {
+  const data = s.split(",");
+  return {
+    time: new Date(formatDateStr(data[0])),
+    host: data[1],
+    response: data[2],
+  };
+});
+const dataGroupByHost = csvArray.reduce((acc, cur) => {
+  if (acc[cur.host] === undefined) acc[cur.host] = [];
+  acc[cur.host].push(cur);
+  return acc;
+}, {});
+
+for (const host in dataGroupByHost) {
+  function* Cursor() {
+    yield* dataGroupByHost[host];
+  }
+
+  const cursor = Cursor();
+  let next = cursor.next();
+  let value = next.value;
+  let done = next.done;
+  let recordWhenBroken = value.response === "-" ? value : null;
+  let timeoutCount = 0;
+
+  while (!done) {
+    if (value.response == "-") {
+      if (timeoutCount === 0) {
+        recordWhenBroken = value;
+      }
+      timeoutCount += 1;
+    }
+    if (
+      recordWhenBroken &&
+      value.response != "-" &&
+      timeoutCount >= timeoutCountMax
+    ) {
+      const returnedDate = new Date(value.time);
+      returnedDate.setMilliseconds(
+        returnedDate.getMilliseconds() + value.response
+      );
+      stdout.push(
+        `サーバー${value.host}が復旧しました。故障期間は${outputDateStr(
+          recordWhenBroken.time
+        )}から${outputDateStr(returnedDate)}までです。\n`
+      );
+      recordWhenBroken = null;
+      timeoutCount = 0;
+    }
+    next = cursor.next();
+    value = next.value;
+    done = next.done;
+  }
+  const sum = dataGroupByHost[host]
+    .slice(-1 * responseCount)
+    .filter((e) => e.response != "-")
+    .reduce((sum, val) => sum + Number(val.response), 0);
+  const count = dataGroupByHost[host]
+    .slice(-1 * responseCount)
+    .filter((e) => e.response != "-").length;
+  const avg = sum / count;
+  if (avg > responseAvg) {
+    stdout.push(
+      `サーバー${host}は過負荷状態です。期間は${outputDateStr(
+        dataGroupByHost[host].slice(-1 * responseCount)[0].time
+      )}からです。\n`
+    );
+  }
+}
+
+const stream = require("stream");
+const ws = new stream.Writable();
+ws._write = function (chunk, encoding, done) {
+  process.send(chunk);
+  done();
+};
+
+stream.Readable.from(stdout).pipe(process.send ? ws : process.stdout);
--- test/exam.test.js
+++ test/exam.test.js
@@ -71,3 +71,49 @@ describe("exam2", () => {
     ).toBe("");
   });
 });
+describe("exam3", () => {
+  test("頂いたcsv", async () => {
+    expect(
+      await getSTDOut(
+        fork("src/exam1.js", ["-p", "test/test.csv"], { silent: true })
+      )
+    ).toBe("");
+  });
+
+  test("test.csv with default", async () => {
+    expect(
+      await getSTDOut(
+        fork("src/exam3.js", ["-p", "test/test3.csv"], { silent: true })
+      )
+    ).toBe(
+      "サーバー10.20.30.1/16が復旧しました。故障期間は2020年1月2日3時4分9.000秒から2020年1月2日3時4分12.100秒までです。\n" +
+        "サーバー10.20.30.2/16は過負荷状態です。期間は2020年1月2日3時4分6.000秒からです。\n" +
+        "サーバー10.20.30.3/16は過負荷状態です。期間は2020年1月2日3時4分8.000秒からです。\n"
+    );
+  });
+  test("test.csv with specified count 6", async () => {
+    expect(
+      await getSTDOut(
+        fork("src/exam3.js", ["-p", "test/test3.csv", "-m", "6"], {
+          silent: true,
+        })
+      )
+    ).toBe(
+      "サーバー10.20.30.1/16が復旧しました。故障期間は2020年1月2日3時4分9.000秒から2020年1月2日3時4分12.100秒までです。\n" +
+        "サーバー10.20.30.2/16は過負荷状態です。期間は2020年1月2日3時4分6.000秒からです。\n" +
+        "サーバー10.20.30.3/16は過負荷状態です。期間は2020年1月2日3時4分8.000秒からです。\n" +
+        "サーバー10.20.30.4/16は過負荷状態です。期間は2020年1月2日3時4分7.000秒からです。\n"
+    );
+  });
+  test("test.csv with specified average 10000", async () => {
+    expect(
+      await getSTDOut(
+        fork("src/exam3.js", ["-p", "test/test3.csv", "-t", "1000"], {
+          silent: true,
+        })
+      )
+    ).toBe(
+      "サーバー10.20.30.1/16が復旧しました。故障期間は2020年1月2日3時4分9.000秒から2020年1月2日3時4分12.100秒までです。\n"
+    );
+  });
+});
